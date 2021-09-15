ae38e5444d34470fd0199fb5147465e7a23b6281
kiwi-fruitiwi <zane.tian@gmail.com>
2021-09-15 13:20:00 -0700

    Add template project

---
+++ sketch.js
@@ -0,0 +1,25 @@
+/*
+@author Kiwi
+@date 2021-09-15
+
+ */
+
+
+let font
+let vehicles = []
+let points = []
+
+function preload() {
+    font = loadFont('fonts/Meiryo-01.ttf');
+}
+
+function setup() {
+    createCanvas(600, 300)
+    colorMode(HSB, 360, 100, 100, 100)
+    background(0, 0, 30)
+}
+
+function draw() {
+    background(0, 0, 30)
+}
+
