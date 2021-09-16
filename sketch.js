40b3b2fd294d9cfc5d5a485d7f99f9f7eafdbcd9
kiwi-fruitiwi <zane.tian@gmail.com>
2021-09-16 13:33:51 -0700

    Add bezier curves, control point demo

--- sketch.js
+++ sketch.js
@@ -10,16 +10,36 @@ let vehicles = []
 let points = []
 
 function preload() {
-    font = loadFont('fonts/Meiryo-01.ttf');
+    font = loadFont('fonts/Meiryo-01.ttf')
 }
 
 function setup() {
-    createCanvas(600, 300)
+    createCanvas(600, 600)
     colorMode(HSB, 360, 100, 100, 100)
     background(0, 0, 30)
 }
 
 function draw() {
     background(0, 0, 30)
+
+    stroke(0, 0, 100)
+    strokeWeight(24)
+    point(0, 300)
+    point(mouseX, mouseY) // control
+    point(400, 400) // control
+    point(600, 300)
+
+    strokeWeight(4)
+    fill(0, 0, 80, 20)
+    beginShape()
+    vertex(0, 300)
+    bezierVertex(mouseX, mouseY, 400, 400, 600, 300)
+    bezierVertex(400, 600, 200, 100, 0, 300)
+    endShape(CLOSE)
+
+    strokeWeight(2)
+    line(0, 300, mouseX, mouseY)
+    line(400, 400, 600, 300)
+
 }
 
