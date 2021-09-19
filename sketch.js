2314a49edcad0cde02e0af8e6952c990a62ea643
kiwi-fruitiwi <zane.tian@gmail.com>
2021-09-19 10:43:07 -0700

    Add Freya's quadratic curve animation

--- sketch.js
+++ sketch.js
@@ -14,8 +14,17 @@ cody: x₀*t+x₁(1-t) » x₁*t+x₀(1-t)
 kiwi: (x₁-x₀)*t + x₀
 
 goal: clone Freya's lerp animation https://youtu.be/aVwxzDHniEw?t=21
-
-
+    white circles are major vertices
+        fill is the background color
+        stroke is white
+        lines are white
+    2nd order vertices are blue
+    3rd order red
+    actual bezier curve is red
+    mouseX « sets t value
+    quadratic example
+        p0, p2 are equilateral triangle bottoms
+        p1 is width/2, height/4
  */
 
 
@@ -23,22 +32,105 @@ let font
 let vehicles = []
 let points = []
 
-// lerp practice!
-let p0, p1, p2, p3
+// a,c are the endpoints
+// d is the result of lerp between a, b
+// e is the result of lerp between b, c
+// f is the lerp between d and e
+let a, b, c, d, e, f
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
 }
 
 function setup() {
-    createCanvas(600, 600)
+    createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
     background(0, 0, 30)
 
-    p0 = new p5.Vector(0, 300)
-    p1 = new p5.Vector(300, 0)
-    p2 = new p5.Vector(400, 0)
-    p3 = new p5.Vector(600, 300)
+    a = new p5.Vector(width/4, height*3/4)
+    b = new p5.Vector(width/2, height/4)
+    c = new p5.Vector(width*3/4, height*3/4)
+
+    // I didn't know we could initialize the value!!
+    mouseX = width/2
+}
+
+function draw() {
+    quadratic_example()
+    cubic_example()
+}
+
+
+// encapsulates the visualization of a cubic bezier curve
+function cubic_example() {
+
+}
+
+
+// encapsulates the visualization of our quadratic bezier curve
+function quadratic_example() {
+    let bg = color(0, 0, 25)
+    background(bg)
+
+    /* TODO z-index order notes
+        the blue circles should be under the white ones
+        the blue lines should disappear as t approaches 0 and 1
+     */
+
+    // for some reason, we have to constrain the mouse
+    // otherwise p5.js allows us to move our mouse off the canvas
+    mouseX = constrain(mouseX, a.x, c.x)
+
+    // main time control for our bezier curve
+    let t = map(mouseX, a.x, c.x, 0, 1)
+    d = p5.Vector.lerp(a, b, t)
+    e = p5.Vector.lerp(b, c, t)
+    f = p5.Vector.lerp(d, e, t)
+
+    noFill()
+
+    // we want the blue 2nd order lerp lines to be under the
+    // 1st order gray
+    stroke(200, 100, 80) // blue
+    line(d.x, d.y, e.x, e.y)
+
+    // draw lines first so they are underneathe the vertices
+    stroke(0, 0, 60) // gray
+    line(a.x, a.y, b.x, b.y)
+    line(b.x, b.y, c.x, c.y)
+
+
+    strokeWeight(2)
+    // a blue circle for each vertex d, e
+    // a line between them
+    fill(bg)
+
+    // if t is close to 0 or 1, reduce the line's alpha to 0
+    // let blue_line_alpha = map(abs(t-0.5), 0, 0.5, 100, 0)
+
+    stroke(200, 100, 80)
+    circle(d.x, d.y, 10)
+    circle(e.x, e.y, 10)
+
+    // draw a path for our curve
+    strokeWeight(3)
+    stroke(0, 0, 100) // white
+    // s is the t value inside this loop, going from 0 up to t itself
+    for (let s=0; s<=t; s+=0.001) {
+        let v = quadratic(a, b, c, s)
+        point(v.x, v.y)
+    }
+
+    // f is our main drawing point for the quadratic bezier curve
+    stroke(0, 0, 100) // white
+    circle(f.x, f.y, 12)
+
+    // a circle for each vertex a, b, c
+    fill(bg)
+    strokeWeight(2)
+    circle(a.x, a.y, 10)
+    circle(b.x, b.y, 10)
+    circle(c.x, c.y, 10)
 }
 
 // receives 3 points, and returns a point on the quadratic
@@ -51,7 +143,7 @@ function quadratic(p0, p1, p2, t) {
     let x = lerp(x1, x2, t)
     let y = lerp(y1, y2, t)
 
-    line(x1, y1, x2, y2)
+    // line(x1, y1, x2, y2)
     return new p5.Vector(x, y)
 }
 
@@ -65,18 +157,3 @@ function cubic(p0, p1, p2, p3, t) {
     line(v1.x, v1.y, v2.x, v2.y)
     return new p5.Vector(x, y)
 }
-
-function draw() {
-    background(0, 0, 30)
-    p1 = new p5.Vector(mouseX, mouseY)
-
-    noFill()
-
-    /* the 1.00001 is a rounding error hack */
-    for (let t=0; t<=1.0001; t+=0.05) {
-        stroke(t*360, 100, 90, 70)
-        let v = cubic(p0, p1, p2, p3, t)
-        vertex(v.x, v.y)
-    }
-}
-
