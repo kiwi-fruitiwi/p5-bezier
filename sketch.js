c210f6720eb21981aed7ac8a779da3e8f22fa761
kiwi-fruitiwi <zane.tian@gmail.com>
2021-09-19 09:33:28 -0700

    Add quadratic+cubic methods

--- sketch.js
+++ sketch.js
@@ -13,6 +13,9 @@ winry: x₀ + x₁*t
 cody: x₀*t+x₁(1-t) » x₁*t+x₀(1-t)
 kiwi: (x₁-x₀)*t + x₀
 
+goal: clone Freya's lerp animation https://youtu.be/aVwxzDHniEw?t=21
+
+
  */
 
 
@@ -21,7 +24,7 @@ let vehicles = []
 let points = []
 
 // lerp practice!
-let p0, p1, p2
+let p0, p1, p2, p3
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
@@ -34,50 +37,46 @@ function setup() {
 
     p0 = new p5.Vector(0, 300)
     p1 = new p5.Vector(300, 0)
-    p2 = new p5.Vector(600, 300)
+    p2 = new p5.Vector(400, 0)
+    p3 = new p5.Vector(600, 300)
+}
+
+// receives 3 points, and returns a point on the quadratic
+// bezier curve
+function quadratic(p0, p1, p2, t) {
+    let x1 = lerp(p0.x, p1.x, t)
+    let y1 = lerp(p0.y, p1.y, t)
+    let x2 = lerp(p1.x, p2.x, t)
+    let y2 = lerp(p1.y, p2.y, t)
+    let x = lerp(x1, x2, t)
+    let y = lerp(y1, y2, t)
+
+    line(x1, y1, x2, y2)
+    return new p5.Vector(x, y)
+}
+
+// two endpoints + two control points » cubic bezier vertex
+function cubic(p0, p1, p2, p3, t) {
+    let v1 = quadratic(p0, p1, p2, t)
+    let v2 = quadratic(p1, p2, p3, t)
+    let x = lerp(v1.x, v2.x, t)
+    let y = lerp(v1.y, v2.y, t)
+
+    line(v1.x, v1.y, v2.x, v2.y)
+    return new p5.Vector(x, y)
 }
 
 function draw() {
     background(0, 0, 30)
     p1 = new p5.Vector(mouseX, mouseY)
 
-    stroke(0, 0, 80)
     noFill()
-    // beginShape()
-    for (let t=0; t<=1.01; t+=0.005) {
-        let x1 = lerp(p0.x, p1.x, t)
-        let y1 = lerp(p0.y, p1.y, t)
-        let x2 = lerp(p1.x, p2.x, t)
-        let y2 = lerp(p1.y, p2.y, t)
 
-        stroke(t*360, 100, 100)
-        line(x1, y1, x2, y2)
-        let x = lerp(x1, x2, t)
-        let y = lerp(y1, y2, t)
-        // point(x, y)
+    /* the 1.00001 is a rounding error hack */
+    for (let t=0; t<=1.0001; t+=0.05) {
+        stroke(t*360, 100, 90, 70)
+        let v = cubic(p0, p1, p2, p3, t)
+        vertex(v.x, v.y)
     }
-
-    // endShape()
-
-
-    // stroke(0, 0, 100)
-    // strokeWeight(24)
-    // point(0, 300)
-    // point(mouseX, mouseY) // control
-    // point(400, 400) // control
-    // point(600, 300)
-    //
-    // strokeWeight(4)
-    // fill(0, 0, 80, 20)
-    // beginShape()
-    // vertex(0, 300)
-    // bezierVertex(mouseX, mouseY, 400, 400, 600, 300)
-    // bezierVertex(400, 600, 200, 100, 0, 300)
-    // endShape(CLOSE)
-    //
-    // strokeWeight(2)
-    // line(0, 300, mouseX, mouseY)
-    // line(400, 400, 600, 300)
-
 }
 
